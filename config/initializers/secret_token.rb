429c75163a85ecfc594bdbd91dc030453bb07331
rozhgart <rozhgar.ahmad@yahoo.com>
2013-05-01 06:15:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RozhgarSample::Application.config.secret_token = 'e28c2e622578a198e90a6a62d97477b513974d8012b11b19d18247786a301bdd74042a3aaf64cffa4a3f295f55497885c8f7588284826eabb04ee2130320d120'
